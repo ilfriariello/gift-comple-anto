4edeae8be6371bf5625262c31a465e2ba8510f27
ilfriariello <arba.webmail@gmail.com>
2024-12-28 04:10:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 window.onload = function () {
     const loadingScreen = document.getElementById('loading-screen');
     const finalContent = document.getElementById('final-content');
+    const music = document.getElementById('music'); // Aggiungi questa riga per selezionare l'audio
 
     // Dopo 5 secondi, passa dalla schermata di caricamento alla pagina finale
     setTimeout(() => {
         loadingScreen.style.display = 'none'; // Nasconde la schermata di caricamento
         finalContent.style.opacity = '1'; // Mostra il contenuto finale con transizione
+        music.play(); // Avvia la riproduzione della canzone (anche se autoplay è già impostato)
     }, 5000); // 5 secondi di caricamento
 };
