0a29c8cc722caa8465e2d45ea6bec96ae6bcb434
ilfriariello <arba.webmail@gmail.com>
2024-12-28 04:38:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-/ Gestione del caricamento e transizione
+// Gestione del caricamento e transizione
 window.onload = function () {
     const loadingScreen = document.getElementById('loading-screen');
     const finalContent = document.getElementById('final-content');
-    const music = document.getElementById('music'); // Seleziona l'elemento audio
-    const message = document.getElementById('message'); // Seleziona il testo
+    const message = document.getElementById('message'); // Seleziona il testo sotto la foto
 
     // Dopo 5 secondi, passa dalla schermata di caricamento alla pagina finale
     setTimeout(() => {
         loadingScreen.style.display = 'none'; // Nasconde la schermata di caricamento
         finalContent.style.opacity = '1'; // Mostra il contenuto finale con transizione
-        music.play(); // Avvia la riproduzione della canzone (anche se autoplay è già impostato)
     }, 5000); // 5 secondi di caricamento
 
     // Dopo 3 secondi dalla visualizzazione del contenuto, mostra il testo
